21f9068885504ae4f2e899d57ef71810ce90bfb1
Beau Johnston <beau@inbeta.org>
2017-06-02 11:29:35 +1000

    added simple python script to generate runs for libscibench plotting

---
+++ opendwarf_miner.py
@@ -0,0 +1,117 @@
+#/usr/bin/env python
+from opendwarf_miner_utils import *
+
+#Benchmark parameters:
+kmeans = {'name':'kmeans',
+          'default':'-i ../test/dense-linear-algebra/kmeans/204800.txt',
+          'full name':'K-Means Clustering'}
+lud = {'name':'lud',
+       'default':'-i ../test/dense-linear-algebra/lud/3072.dat',
+       'full name':'Lower Upper Decomposition'}
+csr = {'name':'csr',
+       'default':'-i ../test/sparse-linear-algebra/SPMV/csr_65536.txt',
+       'full name':'Compressed Sparse Row'}
+fft = {'name':'clfft',
+       'default':'--pts 1',
+       'full name':'Fast Fourier Transform'}
+gem = {'name':'gem',
+       'default':"../test/n-body-methods/gem/nucleosome 80 1 0",
+       'full name':'Gemnoui'}
+srad = {'name':'srad',
+        'default':'256 256 0 127 0 127 0.5 2',
+        'full name':'Speckle Reducing Anisotropic Diffusion'}
+cfd = {'name':'cfd',
+       'default':'../test/unstructured-grids/cfd/fvcorr.domn.097K',
+       'full name':'Computational Fluid Dynamics'}
+crc = {'name':'crc',
+       'default':'-i ../test/combinational-logic/crc/crc_1000x8192.txt',
+       'full name':'Cyclic-Redundancy Check'}
+bfs = {'name':'bfs',
+       'default':'../test/graph-traversal/bfs/graph65536.txt',
+       'full name':'Breadth-First Search'}
+nw = {'name':'needle', 'default':"2048 10", 'full name':'Needleman-Wunsch'}
+swat = {'name':'swat',
+        'default':'../test/dynamic-programming/swat/query1K1 ../test/dynamic-programming/swat/sampledb1K1',
+        'full name':'Smith-Waterman'}
+nqueens = {'name':'nqueens',
+           'default':'20',
+           'full name':'N-Queens'}
+hmm = {'name':'bwa_hmm',
+       'default':'-n 30 -v n',
+       'full name':'Baum-Welch Algorithm, Hidden Markov Model'}
+tdm = {'name':'tdm',
+       'default':'../test/finite-state-machine/tdm/sim-64-size200.csv ../test/finite-state-machine/tdm/ivl.txt ../test/finite-state-machine/tdm/30-episodes.txt 128',
+       'full name':'Temporal Data Mining'}
+
+#Dwarfs as clusters of Benchmarks:
+dense_linear_algebra = [kmeans,lud]
+sparse_linear_algebra = [csr]
+spectral_methods = [fft]
+n_body_methods = [gem]
+structured_grid_methods = [srad]
+unstructured_grid_methods = [cfd]
+combinational_logic = [crc]
+graph_traversal = [bfs]
+dynamic_programming = [nw,swat]
+backtrack_branch_and_bound = [nqueens]
+graphical_models = [hmm]
+finite_state_machines = [tdm]
+
+#System specific device parameters
+cpu_parameters = GenerateDeviceParameters(0,0,0)
+gpu_parameters = GenerateDeviceParameters(2,0,1)
+
+#Sample usage of utils:
+#RunDwarf(dense_linear_algebra,cpu_parameters)
+
+#Sample Usage with Papi to find L1 Cache performance:
+#papi_env=GeneratePAPIParameters("PAPI_L1_TCA", #total L1 cache access
+#                                "PAPI_L1_DCM") #L1 data cache misses
+#RunApplication(kmeans,cpu_parameters,50,papi_env)
+#StoreRun(kmeans,'kmeans_default_l1_cache_misses')
+
+
+#Most common metrics that effect performance as PAPI events:
+#Instructions per cycle (IPC)
+#PAPI_TOT_CYC, PAPI_TOT_INS
+papi_envs = [{'name':'instructions_per_cycle',
+              'parameters':GeneratePAPIParameters('PAPI_TOT_CYC', 'PAPI_TOT_INS')},
+#L1 Cache
+#PAPI_L1_DCA PAPI_L1_DCM
+             {'name':'L1_cache',
+              'parameters':GeneratePAPIParameters('PAPI_L1_DCA', 'PAPI_L1_DCM')},
+#L2 Cache
+#PAPI_L2_DCA PAPI_L2_DCM
+             {'name':'L2_cache',
+              'parameters':GeneratePAPIParameters('PAPI_L2_DCA', 'PAPI_L2_DCM')},
+#L3 Cache (can only use total cache instead of data cache events)
+#PAPI_L3_TCA PAPI_L3_TCM
+             {'name':'L3_cache',
+              'parameters':GeneratePAPIParameters('PAPI_L3_TCA', 'PAPI_L3_TCM')},
+#Translation Lookaside Buffer Misses:
+#PAPI_TLB_DM PAPI_TLB_IM
+             {'name':'translation_lookaside_buffer_misses',
+              'parameters':GeneratePAPIParameters('PAPI_TLB_DM', 'PAPI_TLB_IM')},
+#Branch Mispredictions:
+#PAPI_BR_INS PAPI_BR_MSP
+             {'name':'branch_mispredictions',
+              'parameters':GeneratePAPIParameters('PAPI_BR_INS', 'PAPI_BR_MSP')}
+            ]
+
+#so to find the cache performance steps of kmeans on i7 960@dynamic frequency,
+#over the range of 1.60GHz-3.20GHz:
+for papi_env in papi_envs:
+    RunApplication(kmeans,cpu_parameters,50,papi_env['parameters'])
+    StoreRun(kmeans,'results/kmeans_default_'+papi_env['name'])
+
+#increasing the maximum number of clusters to find increases the amount of
+#computation and thus the run time
+for max_clusters in range(1,12):
+    for papi_env in papi_envs:
+        RunApplicationWithArguments(kmeans,
+                                    kmeans['default']+" -n 1 -m "+str(max_clusters),
+                                    cpu_parameters,
+                                    30,#repeats
+                                    papi_env['parameters'])
+        StoreRun(kmeans,'results/kmeans_'+str(max_clusters)+"_max_clusters_"+papi_env['name'])
+
---
+++ opendwarf_miner_utils.py
@@ -0,0 +1,71 @@
+import os           #to get directory name of the calling script
+import subprocess   #for exec
+import glob         #for wildcards
+import shutil       #to move files
+
+def DMError(error_string):
+    print("*"*80)
+    print(" DwarfMiner Error: "+error_string)
+    print("*"*80)
+
+def GenerateDeviceParameters(platform_id,device_id,type_id):
+    assert type(platform_id) is int, DMError("platform_id is not of type \'int\'")
+    assert type(device_id) is int, DMError("device_id is not of type \'int\'")
+    assert type(type_id) is int, DMError("type_id is not of type \'int\'")
+    return(" -p {} -d {} -t {} -- ".format(platform_id,device_id,type_id))
+
+def GeneratePAPIParameters(papi1,papi2):
+    assert type(papi1) is str, DMError("papi1 is not of type \'str\'")
+    assert type(papi2) is str, DMError("papi1 is not of type \'str\'")
+    return("LSB_PAPI1="+papi1+' LSB_PAPI2='+papi2+" ")
+
+def StoreRun(application,directory):
+    assert type(directory) is str, DMError("directory is not of type \'str\'")
+    i = 0
+    while os.path.exists(directory+".{}".format(i)):
+        i += 1
+    numbered_directory = directory+".{}".format(i)
+
+    os.makedirs(numbered_directory)
+    run_name = application['name']
+    for run in glob.glob('lsb.'+run_name+'.'+'*'):
+        shutil.move(run,numbered_directory)
+
+#def GenerateWorkgroupParameters(one_dimension,two_dimension_x=0,two_dimension_y=0):
+#    assert type(one_dimension) is int, DMError("one_dimension is not of type \'int\'")
+#    assert type(two_dimension_x) is int, DMError("two_dimension_x is not of type \'int\'")
+#    assert type(two_dimension_y) is int, DMError("two_dimension_y is not of type \'int\'")
+#    return("--workgroups \"-local1D {} -local2D {} {}\" ".format(one_dimension,two_dimension_x,two_dimension_y))
+
+def RunApplicationWithArguments(application,arguments,parameters,repeats=1,papi_env=None):
+    command = './'+application['name']+parameters+arguments
+    if papi_env is not None:
+        command = papi_env + command
+    print('Running {}'.format(command))
+    for i in range(0,repeats):
+        if repeats != 1:
+            print('iteration {}'.format(i))
+        process = subprocess.Popen(command,
+                                   shell=True,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        process.wait()
+        if process.returncode != 0:
+            DMError('Application {} failed with {}'.format(application['name'],
+                process.stderr.readlines()))
+
+
+
+def RunApplication(application,parameters,repeats=1,papi_env=None):
+    RunApplicationWithArguments(application,
+                                application['default'],
+                                parameters,
+                                repeats,
+                                papi_env)
+
+def RunDwarf(dwarf,parameters):
+    assert type(dwarf) is list, DMError('dwarf is not of type \'list\'')
+    for application in dwarf:
+        RunApplication(application,parameters)
+
+
